c5781322f2760d69c4815b697f549239c6391d2f
hihi <you@example.com>
2025-06-04 09:59:14 +0900

    first commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorTest {
+
+    @Test
+    void testPrimeFactor1() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        assertNotNull(primeFactor);
+    }
+}
