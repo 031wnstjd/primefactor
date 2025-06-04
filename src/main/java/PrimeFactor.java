01dde5202b29be9fa2fdbdbe0e84c50eb378fb77
hihi <you@example.com>
2025-06-04 10:17:57 +0900

    [refactoring] p36

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -6,10 +6,10 @@ public class PrimeFactor {
     public List<Integer> of(int number) {
         ArrayList<Integer> factors = new ArrayList<>();
         if (number == 2) {
-            factors.add(2);
+            factors.add(number);
         }
         if (number == 3) {
-            factors.add(3);
+            factors.add(number);
         }
 
         return factors;
